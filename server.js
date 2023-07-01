81ec52e9f9dd3c09f81fbe776f790353e9d62180
Ron Buttermore <ron.buttermore@gmail.com>
2023-07-01 06:35:16 -0600

    added paths to homepage and notes page

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
 
 app.listen(PORT, () => 
     console.log(`Serving static asset routes on port ${PORT}`));
